06eb209c227d746bf61ed63c862daed819dd6d4a
stvincentschool25-cpu <stvincent.school25@gmail.com>
2025-12-18 02:53:38 +0530

    Update gtm-loader.js

--- js/gtm-loader.js
+++ js/gtm-loader.js
@@ -3,3 +3,8 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MK9FWB4L');
+// Automatically removes .html from the URL bar without reloading the page
+if (window.location.pathname.endsWith(".html")) {
+    var cleanUrl = window.location.pathname.replace(".html", "");
+    window.history.replaceState(null, null, cleanUrl);
+}
